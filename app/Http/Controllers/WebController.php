53c748c6769069cf21168bd4d3046750f930c553
Chaima434 <79903147+Chaima434@users.noreply.github.com>
2022-03-02 23:51:46 +0100

    add new controller named WebController and lodels

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function Inscription (Request $request)
+    {
+        try
+        {
+        } 
+        catch (Exception $e)
+        {
+        }
+    }
+}
--- app/Service.php
+++ app/Service.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 
-class MyCategory extends Model
+class Service extends Model
 {
     
 }
