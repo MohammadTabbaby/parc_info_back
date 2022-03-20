f336512f30b6055fd0702c22c640fb6593de4ca3
Chaima434 <79903147+Chaima434@users.noreply.github.com>
2022-03-20 23:43:14 +0100

    reference

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ReparationsExterne;
 
 class WebController extends Controller
 {
@@ -13,6 +14,58 @@ class WebController extends Controller
         } 
         catch (Exception $e)
         {
+        }
+    }
+
+    public static function test()
+    {
+        $reparation_externe= ReparationsExterne::all();
+        $tab = [];
+        foreach($reparation_externe as $v)
+        {
+            $tab[] = $v->id_equipement;
+        }
+        return array_unique($tab);
+    }
+
+    public function verifReferenceReparationExterne(Request $request)
+    {
+        try
+        {
+            $r = ReparationExterne::where('id_equipement', $request->id_equipement)->first();
+            if(isset($r))
+            {
+                return response()->json
+                (
+                    [
+                        "code" => 0,
+                        "status" => "error",
+                        "message" => "Equipement existe"
+                    ]
+                );
+            }
+            else
+            {
+                return response()->json
+                (
+                    [
+                        "code" => 1,
+                        "status" => "sucess",
+                        "message" => "Equipement non existe"
+                    ]
+                );
+            }
+        }
+        catch(Exception $e)
+        {
+            return response()->json
+            (
+                [
+                    "code" => 0,
+                    "status" => "exception",
+                    "message" => "Exception"
+                ]
+            );
         }
     }
 }
