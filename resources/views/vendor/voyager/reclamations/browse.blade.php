498da5a2cb2737935a1b371ad91e9a8d4f1926f0
Chaima434 <79903147+Chaima434@users.noreply.github.com>
2022-04-27 00:50:48 +0100

    rectification Reclamation

--- resources/views/vendor/voyager/reclamations/browse.blade.php
+++ resources/views/vendor/voyager/reclamations/browse.blade.php
@@ -15,12 +15,6 @@
             <i class="voyager-list"></i> <span>Add Reclamation </span>
         </a>
         @endif
-        
-        @can('add', app($dataType->model_name))
-            <a href="{{ route('voyager.'.$dataType->slug.'.create') }}" class="btn btn-success btn-add-new">
-                <i class="voyager-plus"></i> <span>{{ __('voyager::generic.add_new') }}</span>
-            </a>
-        @endcan
         @can('delete', app($dataType->model_name))
             @include('voyager::partials.bulk-delete')
         @endcan
