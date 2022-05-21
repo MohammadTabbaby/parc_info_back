e069d76f3e5d94894f6f2b5525a9c02ba6546e3b
Chaima434 <79903147+Chaima434@users.noreply.github.com>
2022-05-21 13:40:13 +0100

    Update browse.blade.php

--- resources/views/vendor/voyager/dashboards/browse.blade.php
+++ resources/views/vendor/voyager/dashboards/browse.blade.php
@@ -233,10 +233,10 @@
                                     </div>
                                     <!--/Services-->
                                     <div class="flex flex-row flex-wrap flex-grow mt-2">
-                                   
+                                        <div style="width:100%">
                                     
                     
-                                    <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                                    
                                         <!--Graph Card-->
                                         <div class="bg-white border-transparent rounded-lg shadow-xl">
                                             <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
