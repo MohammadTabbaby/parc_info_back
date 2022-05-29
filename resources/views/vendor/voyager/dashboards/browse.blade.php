47023a5792fb42d9645dc20a59b938cafeaefbc6
Chaima434 <79903147+Chaima434@users.noreply.github.com>
2022-05-29 12:13:54 +0100

    dashboard

--- resources/views/vendor/voyager/dashboards/browse.blade.php
+++ resources/views/vendor/voyager/dashboards/browse.blade.php
@@ -87,13 +87,13 @@
                                                     }
                                                     ?>
                                                 </ul>
+                
+                                      
                                             </div>
                                             <!--/Metric Card-->
                                         </div>
 
-                                       
-
-
+                                        
                                         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                                             <!--Metric Card-->
                                             <div class="bg-gradient-to-b from-red-200 to-red-100 border-b-4 border-red-500 rounded-lg shadow-xl p-5">
@@ -117,8 +117,7 @@
                                                 <br>
                                                 <br>
                                                 <br>
-                                                <br>
-                                                <br>
+                                         
                                             </div>
                                             <!--/Metric Card-->
                                         </div>
@@ -146,13 +145,11 @@
                                                 <br>
                                                 <br>
                                                 <br>
-                                                <br>
-                                                <br>
+                                            
                                             </div>
                                             <!--/Metric Card-->
                                         </div>
 
- 
 
                                         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                                             <!--Metric Card-->
@@ -177,16 +174,20 @@
 
                                                 </div>
                                                 <br>
-                                                <br>
-                                                <br>
-                                                <br>
-                                                <br>
-                                                <br>
-                                                <br>
+                                               <br>
+                                               <br>
+                                               <br>
+                                               
+                                                
+                                                <?php
+                                                    $Result = App\Http\Controllers\WebController::getUsedPiece();
+                                                    echo ("<h2 style='font-size:25px' class='font-bold uppercase text-gray-800'>Disponibles : ".$Result."</h2>");
+                                                ?>
                                                 
                                             </div>
                                             <!--/Metric Card-->
                                         </div>
+
 
                                         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                                             <!--Metric Card-->
@@ -214,8 +215,6 @@
                                                 <br>
                                                 <br>
                                                 <br>
-                                                <br>
-                                                <br>
                                                 
 
                                                 <?php
@@ -226,7 +225,6 @@
                                             <!--/Metric Card-->
                                         </div>
 
-                                        
 
                                         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                                             <!--Metric Card-->
@@ -254,8 +252,6 @@
                                                <br>
                                                <br>
                                                <br>
-                                               <br>
-                                               <br>
                                                 
                                                 <?php
                                                     $tab = App\Http\Controllers\WebController::getBesoinsEnattente();
@@ -263,17 +259,16 @@
                                                 ?>
                                              </div>
                                             <!--/Metric Card-->
+                                            </div>
+
                                         </div>
 
-                                       
-
-                                    </div>
                                 </div>
-                                    <br>  
-                                    <br>
 
-                                    <div class="flex flex-row flex-wrap flex-grow mt-2">
-                                        <div style="width:50%">
+                                <div class="flex flex-row flex-wrap flex-grow mt-2">
+
+
+                                    <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                                         <!--Graph Card-->
                                         <div class="bg-white border-transparent rounded-lg shadow-xl">
                                             <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
@@ -310,8 +305,57 @@
                                         </div>
                                         <!--/Graph Card-->
                                     </div>
+                    
+                                    <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                                        <!--Graph Card-->
+                                        <div class="bg-white border-transparent rounded-lg shadow-xl">
+                                            <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
+                                                <h5 class="font-bold uppercase text-gray-600">Graph</h5>
+                                            </div>
+                                            <div class="p-5"><canvas id="chartjs-4" class="chartjs" width="undefined" height="undefined"></canvas>
+                                                <script>
+                                                    new Chart(document.getElementById("chartjs-4"), {
+                                                        "type": "doughnut",
+                                                        "data": {
+                                                            "labels": ["P1", "P2", "P3"],
+                                                            "datasets": [{
+                                                                "label": "Issues",
+                                                                "data": [300, 50, 100],
+                                                                "backgroundColor": ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
+                                                            }]
+                                                        }
+                                                    });
+                                                </script>
+                                            </div>
+                                        </div>
+                                        <!--/Graph Card-->
+                                    </div>
 
-                                    <!--/Services-->
+                                    <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+                                        <!--Graph Card-->
+                                        <div class="bg-white border-transparent rounded-lg shadow-xl">
+                                            <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
+                                                <h5 class="font-bold uppercase text-gray-600">Graph</h5>
+                                            </div>
+                                            <div class="p-5"><canvas id="chartjs-4" class="chartjs" width="undefined" height="undefined"></canvas>
+                                                <script>
+                                                    new Chart(document.getElementById("chartjs-4"), {
+                                                        "type": "doughnut",
+                                                        "data": {
+                                                            "labels": ["P1", "P2", "P3"],
+                                                            "datasets": [{
+                                                                "label": "Issues",
+                                                                "data": [300, 50, 100],
+                                                                "backgroundColor": ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
+                                                            }]
+                                                        }
+                                                    });
+                                                </script>
+                                            </div>
+                                        </div>
+                                        <!--/Graph Card-->
+                                    </div>
+                                     <!--Services-->
                                     <div class="flex flex-row flex-wrap flex-grow mt-2">
                                         <div style="width:100%">
                                             <!--Graph Card-->
@@ -354,13 +398,13 @@
                                             </div>
                                             <!--/Graph Card-->
                                         </div>
-                                        </div>
+                                    </div>
                                         <!--/Services-->
-                                 
-        
-                              
-                            
-                             </section>
+
+
+                                </div>
+
+                            </section>
                     
                         </div>
                         @if ($isServerSide)
